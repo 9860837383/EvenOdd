e0293ece5bb681242b781708926cadb99d85be76
Pratiksha <shelkepratiksha12@gmail.com>
2023-02-02 09:31:23 +0530

    First Version

---
+++ src/Main/EvenOdd.java
@@ -0,0 +1,15 @@
+package Main;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+
+	        int num = 23;
+
+	        String evenOdd = (num % 2 == 0) ? "even" : "odd";
+
+	        System.out.println(num + " is " + evenOdd);
+
+	}
+
+}
